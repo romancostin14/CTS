e7f6f7ce27b50d0ed942fb014eb19f5803a84519
romancostin14 <80033012+romancostin14@users.noreply.github.com>
2021-03-04 14:19:43 +0200

    Add files via upload

---
+++ CTS_seminar/src/ro/ase/csie/cts/seminar2/Main.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.seminar2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			CalendarUtil cu = new CalendarUtil();
+			System.out.println(cu.weekDay(3));
+	}
+
+}
